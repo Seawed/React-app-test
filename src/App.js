262b0837d7cc1ab2275f2b422408e85d8a7ba8e3
Seawed <seawed@bk.ru>
2023-05-05 22:24:11 +0300

    asdasd

--- src/App.js
+++ src/App.js
@@ -28,9 +28,9 @@ function App() {
         </a>{' '}
         was bootstrapped with{' '}
         <a href="https://facebook.github.io/create-react-app/">
-          Create React App
+          Create React AppPLEASE
         </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
+        and , <code>/public</code> for static assets,{' '}
         <code>/src</code> for components and content, and <code>/api</code>{' '}
         which contains a serverless <a href="https://golang.org/">Go</a>{' '}
         function. See{' '}
